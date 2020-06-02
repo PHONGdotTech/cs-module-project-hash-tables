b36a8ce141352602c0ac1bd0b7bcc8d354fae601
Phong Yang <phongyang88@hotmail.com>
2020-06-02 17:58:18 -0500

    removed linkedList class as it wasnt really necessary and was only really used for access to the linkedList head and by default each slot's first entry is the head

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -11,84 +11,6 @@ class HashTableEntry:
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
 
-class LinkedList:
-    def __init__(self):
-        # first node in the list 
-        self.head = None
-        self.tail = None
-
-    def add_to_end(self, value):
-        new_node = Node(value)
-        if not self.head and not self.tail:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.set_next(new_node)
-            self.tail = new_node
-    
-    def remove_from_end(self):
-        # what if the list is empty? 
-        if not self.head and not self.tail:
-            return None
-        # what if the list isn't empty?
-        else:
-            # what node do we want to remove the last node from? 
-            # the last node in the list. keep track with current
-            current = self.head
-            # use previous to keep track of 2nd to last
-            previous = current
-            # if the head does not have a next node, it is the only node, so remove it
-            if current.get_next() is None:
-                self.head = None
-                self.tail = None
-            else:
-                # traverse the list to get the last and next to last element
-                while current.get_next() is not None:
-                    # before assigning next to current, assign current to previous
-                    previous = current
-                    current = current.get_next()
-                # we're at the end of the linked list
-                # set the previous's next node to none to remove the current from list
-                previous.set_next(None)
-            # return current.value to be printed
-            # current.value is head if the only node, else it is last node
-            return current.value
-
-    def add_to_head(self, key, value):
-        new_node = HashTableEntry(key, value)
-
-        if not self.head and not self.tail:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.set_next(self.head)
-            self.head = new_node
-    
-    def remove_from_head(self):
-        # what if the list is empty?
-        if not self.head:
-            return None
-        # what if it isn't empty?
-        else:
-            # we want to return the value at the current head 
-            value = self.head.get_value()
-            # remove the value at the head 
-            # update self.head 
-            self.head = self.head.get_next()
-            return value
-
-    def get_length(self):
-        if not self.head:
-            return 0
-        else:
-            current = self.head
-            length = 1
-            while current.get_next() is not None:
-                current = current.get_next()
-                length = length+1
-            return length
-
-
 class HashTable:
     """
     A hash table that with `capacity` buckets
@@ -170,11 +92,9 @@ class HashTable:
         slot = self.hash_index(key)
         if self.data[slot] is None:
             self.used_slots += 1
-            ll = LinkedList()
-            ll.add_to_head(key, value)
-            self.data[slot] = ll
+            self.data[slot] = HashTableEntry(key, value)
         else:
-            current = self.data[slot].head
+            current = self.data[slot]
             prev = current
             while current is not None:
                 if current.key == key:
@@ -202,12 +122,12 @@ class HashTable:
         if hash_entry is None:
             return None
         else:
-            current = self.data[slot].head
+            current = self.data[slot]
             prev = current
 
-            if self.data[slot].head.key == key:
+            if self.data[slot].key == key:
                 value = current.value
-                self.data[slot].head = current.next
+                self.data[slot] = current.next
                 self.used_slots -= 1
                 return value
             else:
@@ -236,7 +156,7 @@ class HashTable:
         if hash_entry is None:
             return
         else:
-            current = self.data[slot].head
+            current = self.data[slot]
             while current is not None:
                 if current.key == key:
                     return current.value
@@ -255,7 +175,7 @@ class HashTable:
         ht = HashTable(new_capacity)
         for slot in self.data:
             if slot is not None:
-                current = slot.head
+                current = slot
                 while current is not None:
                     ht.put(current.key, current.value)
                     current = current.next
